23e53ddb5530245b2d79669f99854acd95744be2
chike <chike@sfc.wide.ad.jp>
2017-06-05 21:22:17 +0900

    make ascii name and hash

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -15,7 +15,24 @@ class PrintController extends Controller
             "name" => $request->objname,
             "hash" => $request->gcodehash,
             "ApiKey" => $request->apikey
-        ];
+            ];
+
+        //make Transaction data
+        $nameascii = '';
+        $i = 0;
+        while (($char = substr($data["name"], $i++, 1)) !=='') {
+            $nameascii .= sprintf('%x', ord($char));
+        }
+        $namelen = dechex(strlen($data["name"]));
+        $namelen = str_pad($namelen, 64, 0, STR_PAD_LEFT);
+        $nameascii = str_pad($nameascii, 64, 0, STR_PAD_RIGHT);
+        $hashascii = '';
+        $i = 0;
+        while (($char = substr($data["hash"], $i++, 1)) !== '') {
+            $hashascii .= sprintf('%x', ord($char));
+        }
+
+        //printing
         return Response::json($data);
     }
 }
